066f559736ce226d49539ebcd3870a14f25b2b2e
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-03-07 14:12:49 -0600

    trying to add background image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+//Importing background to set background image// 
+import background from './background.png';
+
+
 
 function App() {
   return (
