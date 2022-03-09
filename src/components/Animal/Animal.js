9b1073517727eda1742b4c6865592b786faadc8b
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-03-08 19:19:55 -0600

    finishing touches

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -3,7 +3,7 @@ import './Animal.css';
 
 export default function Animal(props) {
   return (
-    <div className='animal'>
+    <div className='animal' style={{ top: props.top, left: props.left }}>
       <img alt={props.name} style={{ height: '50px' }} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       <span className='name'>{props.name}</span>
       <span>{props.says}</span>
